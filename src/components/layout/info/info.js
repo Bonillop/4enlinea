6c84fea6bb7c5a2a5cd14a8c6cc17fff9848916e
Pablo <pbonillo@moorea.io>
2019-06-26 17:22:39 -0300

    Mejorada la validación y completa la funcionalidad del 4 en linea, podría optimizarse y mejorarse la estética

---
+++ src/components/layout/info/info.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./info.module.css";
+
+const info = props => {
+  return (
+    <div className={classes.info}>
+      <div>
+        <h2>Siguiente Turno: </h2>
+      </div>
+      <div>
+        {props.whiteTurn ? props.estadosPosibles[2] : props.estadosPosibles[1]}
+      </div>
+      {props.hayGanador ? (
+        <div>
+          <h2>Ganó {props.ganador}</h2>
+          <button onClick={() => props.reiniciarJuego()}>reiniciar</button>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default info;
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Grilla from "../grilla/grilla";
+import Info from "./info/info";
 import classes from "./layout.module.css";
 
 class Layout extends Component {
@@ -7,30 +8,60 @@ class Layout extends Component {
     super(props);
 
     this.state = {
+      hayGanador: false,
+      ganador: "",
+      whiteTurn: true,
       numGrillas: 1,
       numFilas: 8,
       numColumnas: 8,
+      sinMovimientos: false,
       /**
-       * Para utilizar una imagen pasar directamente el tag img con la classname imagen y el 
+       * Para utilizar una imagen pasar directamente el tag img con la classname imagen y el
        * src dentro de la carpeta assets y la imagen. Ejemplo:
        */
-      estadosPosibles: [" ", "X", "O",
-        <img className={classes.imagen} alt='patita' src='/assets/patita.svg' />,
-        <img className={classes.imagen} alt='patita' src='/assets/hot-dog.svg' />],
+      estadosPosibles: [
+        " ",
+        <img className={classes.imagen} alt="black" src="/assets/black.svg" />,
+        <img className={classes.imagen} alt="white" src="/assets/white.svg" />
+      ],
       estadosCeldas: []
     };
   }
 
-  inicializarEstados() {
-    let estadosCeldas = new Array(this.state.numColumnas * this.state.numFilas);
-    for (let i = 0; i < estadosCeldas.length; ++i) {
-      estadosCeldas[i] = 0;
+  reiniciarJuego = () => {
+    let nuevoEstado = { ...this.state };
+
+    nuevoEstado.hayGanador = false;
+    nuevoEstado.ganador = "";
+    nuevoEstado.whiteTurn = true;
+    nuevoEstado.estadosCeldas.fill(0);
+    this.setState(nuevoEstado);
+  };
+
+  checkSinMovimientos() {
+    let sinMovimientos;
+    if (!this.state.estadosCeldas.includes(0)) {
+      sinMovimientos = true;
+      this.setState({ sinMovimientos });
     }
+  }
+
+  inicializarEstados() {
+    let estadosCeldas = new Array(
+      this.state.numColumnas * this.state.numFilas
+    ).fill(0);
     this.setState({ estadosCeldas });
   }
 
   componentDidMount() {
     this.inicializarEstados();
+  }
+
+  componentDidUpdate() {
+    console.log("pepe");
+    if (!this.state.sinMovimientos) {
+      this.checkSinMovimientos();
+    }
   }
 
   getArrayIndex = (fila, columna) => {
@@ -39,25 +70,137 @@ class Layout extends Component {
 
   handleGetValue = (fila, columna) => {
     let arrayIndex = this.getArrayIndex(fila, columna);
-    let value = this.state.estadosCeldas[arrayIndex]
+    let value = this.state.estadosCeldas[arrayIndex];
     return this.state.estadosPosibles[value];
   };
 
   handleClick = (fila, columna) => {
-    let arrayIndex = this.getArrayIndex(fila, columna);
+    if (!this.state.hayGanador) {
+      console.time("pepe");
+      this.ponerFicha(this.getArrayIndex(fila, columna));
+    }
+  };
+
+  ponerFicha = arrayIndex => {
     let estadoActual = this.state.estadosCeldas[arrayIndex];
+    let offset = arrayIndex + this.state.numColumnas;
     let nuevoEstado = [...this.state.estadosCeldas];
 
-    for (let key in this.state.estadosPosibles) {
-      if (estadoActual === +key) {
-        if (this.state.estadosPosibles[+key + 1]) {
-          nuevoEstado[arrayIndex] = +key + 1;
+    if (estadoActual === 0) {
+      if (nuevoEstado[offset] === 0) {
+        this.ponerFicha(offset);
+      } else {
+        if (this.state.whiteTurn) {
+          nuevoEstado[arrayIndex] = 2;
         } else {
-          nuevoEstado[arrayIndex] = 0;
+          nuevoEstado[arrayIndex] = 1;
+        }
+        let turnoActual = this.state.whiteTurn;
+        let siguienteTurno = !this.state.whiteTurn;
+        this.setState({
+          estadosCeldas: nuevoEstado,
+          whiteTurn: siguienteTurno
+        });
+
+        if (this.hayGanador(nuevoEstado, arrayIndex)) {
+          let ganador;
+          if (turnoActual) {
+            ganador = "Blanco";
+          } else {
+            ganador = "Negro";
+          }
+          this.setState({ hayGanador: true, ganador: ganador });
+        }
+        console.timeEnd("pepe");
+      }
+    }
+  };
+
+  checkDerecha(nuevoEstado, arrayIndex) {
+    /**
+     *  Para los casos en los que hay un desplazamiento horizontal, es importante validar tambien la fila en la que se 
+     * encuentra la última posicion a validar, ya que al trabajar con un array, el offset se puede ver afectado por el
+     * desplazamiento horizontal en los casos que se dibuja en los límites de la grilla. Es medio feo pero funciona
+     */ 
+    if (
+      nuevoEstado[+arrayIndex] === nuevoEstado[+arrayIndex + 3] &&
+      Math.floor(+arrayIndex / this.state.numColumnas) ===
+        Math.floor((+arrayIndex + 3) / this.state.numColumnas)
+    ) {
+      if (
+        nuevoEstado[+arrayIndex] === nuevoEstado[+arrayIndex + 2] &&
+        nuevoEstado[+arrayIndex] === nuevoEstado[+arrayIndex + 1]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkAbajoDerecha(nuevoEstado, arrayIndex, offset) {
+    if (
+      nuevoEstado[+arrayIndex] === nuevoEstado[+arrayIndex + offset * 3 + 3] &&
+      Math.floor(+arrayIndex / this.state.numColumnas) ===
+        Math.floor((+arrayIndex + offset * 3 + 3) /this.state.numColumnas) - 3
+    ) {
+      if (
+        nuevoEstado[+arrayIndex] ===
+          nuevoEstado[+arrayIndex + offset * 2 + 2] &&
+        nuevoEstado[+arrayIndex] === nuevoEstado[+arrayIndex + offset + 1]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkAbajo(nuevoEstado, arrayIndex, offset) {
+    if (nuevoEstado[+arrayIndex] === nuevoEstado[+arrayIndex + offset * 3]) {
+      if (
+        nuevoEstado[+arrayIndex] === nuevoEstado[+arrayIndex + offset * 2] &&
+        nuevoEstado[+arrayIndex] === nuevoEstado[+arrayIndex + offset]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkAbajoIzquierda(nuevoEstado, arrayIndex, offset) {
+    if (
+      nuevoEstado[+arrayIndex] === nuevoEstado[+arrayIndex + offset * 3 - 3] &&
+      Math.floor(+arrayIndex / this.state.numColumnas) ===
+      Math.floor((+arrayIndex + offset * 3 - 3) /this.state.numColumnas) - 3
+    ) {
+      if (
+        nuevoEstado[+arrayIndex] ===
+          nuevoEstado[+arrayIndex + offset * 2 - 2] &&
+        nuevoEstado[+arrayIndex] === nuevoEstado[+arrayIndex + offset - 1]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  hayGanador = (nuevoEstado, arrayIndex) => {
+    let offset = this.state.numColumnas;
+    // let valorFicha = nuevoEstado[arrayIndex];
+    // let existencias = 0;
+
+    for (let arrayIndex in nuevoEstado) {
+      if (nuevoEstado[+arrayIndex] !== 0) {
+        if (
+          this.checkDerecha(nuevoEstado, +arrayIndex) ||
+          this.checkAbajoDerecha(nuevoEstado, +arrayIndex, offset) ||
+          this.checkAbajo(nuevoEstado, +arrayIndex, offset) ||
+          this.checkAbajoIzquierda(nuevoEstado, +arrayIndex, offset)
+        ) {
+          return true;
         }
       }
     }
-    this.setState({ estadosCeldas: nuevoEstado });
+    return false;
   };
 
   render() {
@@ -65,6 +208,13 @@ class Layout extends Component {
 
     return (
       <div className={classes.layout}>
+        <Info
+          whiteTurn={this.state.whiteTurn}
+          estadosPosibles={this.state.estadosPosibles}
+          hayGanador={this.state.hayGanador}
+          ganador={this.state.ganador}
+          reiniciarJuego={this.reiniciarJuego}
+        />
         {grillas.map((value, index) => {
           return (
             <Grilla
